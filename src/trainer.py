2127324a30b08dcb2fefe802cf3e69cb6208952a
amr asmar <s12324208@stu.najah.edu>
2026-05-21 13:26:40 +0000

    mobilenet model

---
+++ src/trainer.py
@@ -0,0 +1,83 @@
+import torch
+
+def train_model(model,train_loader,val_loader,criterion,optimizer,epochs,device,model_path):
+
+    best_acc = 0
+    history = {
+        "train_loss": [],
+        "train_acc": [],
+        "val_loss": [],
+        "val_acc": []
+    }
+    for epoch in range(epochs):
+
+        model.train()
+        train_loss = 0
+        train_correct = 0
+        train_total = 0
+
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs,labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+            _, predicted = torch.max(outputs,1)
+
+            train_total += labels.size(0)
+            train_correct += (predicted == labels).sum().item()
+
+        train_loss /= len(train_loader)
+        train_acc = (train_correct/train_total)
+
+
+        model.eval()
+        val_loss = 0
+        val_correct = 0
+        val_total = 0
+
+        with torch.no_grad():
+            for images, labels in val_loader:
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = model(images)
+                loss = criterion(outputs,labels)
+                val_loss += loss.item()
+                _, predicted = torch.max(outputs,1)
+                val_total += labels.size(0)
+                val_correct += (predicted == labels).sum().item()
+
+        val_loss /= len(val_loader)
+        val_acc = (val_correct/val_total)
+
+        # ==========================
+        # SAVE HISTORY
+        # ==========================
+
+        history["train_loss"].append(train_loss)
+        history["train_acc"].append(train_acc)
+        history["val_loss"].append(val_loss)
+        history["val_acc"].append(val_acc)
+
+        # ==========================
+        # SAVE BEST MODEL
+        # ==========================
+
+        if val_acc > best_acc:
+            best_acc = val_acc
+            torch.save(model.state_dict(),model_path)
+            print(f"Model updated | Best Val Acc: {best_acc:.4f}")
+
+        # ==========================
+        # PRINT
+        # ==========================
+
+        print(f"Epoch [{epoch+1}/{epochs}]")
+        print(f"Train Loss: {train_loss:.4f} | Train Acc: {train_acc:.4f}")
+        print(f"Val Loss: {val_loss:.4f} | Val Acc: {val_acc:.4f}")
+        print("-" * 50)
+
+    return best_acc, history
--- src/transforms.py
+++ src/transforms.py
@@ -1,23 +1,23 @@
 from torchvision import transforms
 
-IMG_SIZE = 224
-
+# ---------------- TRAIN TRANSFORMS ----------------
 train_transform = transforms.Compose([
-    transforms.Resize((IMG_SIZE, IMG_SIZE)),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(10),
-    transforms.ToTensor(),
+    transforms.Resize((224, 224)),   
+    transforms.RandomHorizontalFlip(),  
+    transforms.RandomRotation(10),      
+    transforms.ToTensor(),              
     transforms.Normalize(
-        [0.5, 0.5, 0.5],
-        [0.5, 0.5, 0.5]
+        mean=[0.485, 0.456, 0.406],     
+        std=[0.229, 0.224, 0.225]
     )
 ])
 
+# ---------------- TEST / VALIDATION TRANSFORMS ----------------
 test_transform = transforms.Compose([
-    transforms.Resize((IMG_SIZE, IMG_SIZE)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(
-        [0.5, 0.5, 0.5],
-        [0.5, 0.5, 0.5]
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
     )
 ])
